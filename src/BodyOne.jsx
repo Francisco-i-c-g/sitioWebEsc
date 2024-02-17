97839769663ba4e5a12c5043b8154b514c068dc4
Francisco Israel <francisco.iii.ccc.ggg@gmail.com>
2024-02-17 03:50:58 -0600

    aplicando el estilo de oscurecimiento con letras en la imagen 4

--- src/BodyOne.jsx
+++ src/BodyOne.jsx
@@ -4,7 +4,29 @@ import img2 from "./imgBody/imagenEducacion4.jpg";
 import img3 from "./imgBody/estudiantesImg.jpg";
 import img4 from "./imgBody/prapaImg.webp";
 
+const originalStyle={
+  width: "650px", 
+  height: "450px" 
+
+}
+
+const CostumStyle ={
+  width: "800px", 
+  height: "450px",
+  filter: "brightness(30%)"
+  
+
+
+}
+
 export default function Body() {
+  const [imagenOri, setImagenOri] = useState(imagen2(img4, "imagen4", originalStyle))
+  
+
+  function aplicarEfectos(img){
+    setImagenOri(img)
+  }
+
   return (
     <div className="container">
       <div className="row mb-3 mt-5">
@@ -13,7 +35,14 @@ export default function Body() {
       </div>
       <div className="row mb-5">
         {imagen(img3, "imagen3")}
-        {imagen(img4, "imagen4")}
+        <div 
+          style={{position:"relative", textAlign:"center", color:"white"}}
+          className="col-sm-6 mb-3 parentContainer" 
+          onMouseOver={() =>aplicarEfectos(imagen2(img4, "imagen5", CostumStyle,letrasEnImagen("CENTRO UNIVERSITARIO")))}
+          onMouseOut={() =>aplicarEfectos(imagen2(img4, "imagen5", originalStyle ))}  
+          >
+        {imagenOri}
+        </div>
       </div>
     </div>
   );
@@ -26,8 +55,36 @@ function imagen(img, alt) {
         src={img}
         className="img-fluid"
         alt={alt}
-        style={{ width: "650px", height: "450px" }}
+        style={originalStyle}
       />
     </div>
   );
 }
+
+function imagen2(img, alt, styles, prop) {
+  return (
+    <>
+      <img
+        src={img}
+        className="img-fluid"
+        alt={alt}
+        style={styles}
+      />
+
+      {prop}
+      
+      </>
+
+    
+  );
+}
+
+function letrasEnImagen(texto){
+
+  return(
+    <div style={{position:"absolute", top:"50%", left:"50%", transform: "translate(-50%, -50%)", fontSize:"25px"}}>{texto}</div>
+  );
+
+}
+
+
